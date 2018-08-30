27ef15f0ae9a5edd063054d2a8a85cc7cae94565
Brandon LeBlanc <brandon@leblanc.codes>
2018-08-30 13:22:00 -0500

    add module to parse **all** of platform's environment variables

---
+++ dude/platform.py
@@ -0,0 +1,156 @@
+"""A module to process the Platform.sh environment."""
+
+import base64
+import json
+import os
+import shlex
+from functools import partial
+from itertools import chain, tee
+
+_json_vars = (
+    'PLATFORM_APPLICATION',
+    'PLATFORM_VARIABLES',
+    'PLATFORM_RELATIONSHIPS',
+    'PLATFORM_ROUTES',
+)
+
+
+def decode_json(data):
+    """Decode a base64 encoded json string."""
+    data = os.fsencode(data)
+    data = base64.b64decode(data)
+    return json.loads(data, object_pairs_hook=attrdict)
+
+
+def make_name(key):
+    """Return a string suitable for use as a python identifer from an environment key."""
+    return key.replace('PLATFORM_', '').lower()
+
+
+class attrdict(dict):  # noqa: N801 # PascalCase warning
+    """A `dict` that exposes its keys as attributes."""
+
+    __slots__ = ()
+
+    def __getattr__(self, name):
+        """Return the key if it exists otherwise raise an AttributeError."""
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __dir__(self):
+        """Return the dir() listing for this object.
+
+        Includes the `dict` keys as they are exposed as attributes.
+        """
+        return chain(super(attrdict, self).__dir__(), self)
+
+
+class EnvDescriptor(object):
+    """A Member Descriptor to fetch an environment variable."""
+
+    def __init__(self, variable, convert=None):
+        """Initialize the descriptor with a variable name and converter function.
+
+        The default `convert` function decodes the byte string from the operating
+        system into a str.
+        """
+        self.variable = os.fsencode(variable)
+        self.convert = convert or os.fsdecode
+
+    def __get__(self, inst, owner=None):
+        """Get the `self.variable` from the environment and apply the converter."""
+        rv = os.getenvb(self.variable)
+        if rv:
+            return self.convert(rv)
+
+
+class Environment(object):
+    """A mixin class for the environment returned from `build_environment`."""
+
+    __slots__ = ('_keys')
+
+    def __init__(self, keys):
+        """Initialize the environment object.
+
+        The environment is provided with a list of its keys at initialization to
+        speed up other string formatting functions.
+        """
+        self._keys = keys
+
+    def __repr__(self):
+        """Represent the environment in a human friendly way for debugging."""
+        return '{}({})'.format(self.__class__.__name__, ', '.join(self._keys))
+
+    def __str__(self):
+        """Format the environment as `.env` file."""
+        def quote(obj):
+            if not isinstance(obj, str):
+                obj = json.dumps(obj, sort_keys=True, separators=(',', ':'))
+                obj = obj.encode('ascii')
+                obj = base64.b64encode(obj)
+                obj = obj.decode('ascii')
+
+            return shlex.quote(obj)
+
+        keys, values = tee(self._keys)
+        values = map(make_name, values)
+        values = map(partial(getattr, self), values)
+        values = map(quote, values)
+        items = map('declare -x {}={};\n'.format, keys, values)
+        return ''.join(items)
+
+
+def build_environment(*extra_keys, **kwargs):
+    """Build an object with the Platform.sh environment variables as properties.
+
+    The function accepts extra environment variables as positional parameters.
+    The function also decodes base64-json encoded variables provided as keyword
+    arguments. JSON objects are decoded into `attrdict` objects.
+
+    Example:
+
+        >>> import os
+        >>> import base64
+        >>> import json
+        >>> from dude.platform import build_environment
+        >>> os.environ['SENDGRID_TOKEN'] = 'test'
+        >>> os.environb[b'METADATA'] = base64.b64encode(os.fsencode(json.dumps({'test': True})))
+        >>> environment = build_environment('SENDGRID_TOKEN', METADATA=True)
+        >>> environment.application_name
+        'dude'
+        >>> environment.sendgrid_token
+        'test'
+        >>> environment.metadata
+        {'test': True}
+        >>> environment.metadata.test
+        True
+    """
+    keys = [
+        key
+        for key in os.environ.keys()
+        if key.startswith('PLATFORM')
+        or key in extra_keys
+        or key in kwargs
+    ]
+
+    is_json = set(_json_vars)
+    is_json.update(kwargs)
+
+    props = {
+        make_name(key):
+        EnvDescriptor(key, decode_json if key in is_json else None)
+        for key in keys
+    }
+
+    klass = type(
+        'PlatformEnvironment',
+        (Environment, object, ),
+        props
+    )
+
+    return klass(keys)
+
+
+environment = build_environment()
